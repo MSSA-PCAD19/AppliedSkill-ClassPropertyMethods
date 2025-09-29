765226c874571f5769b713f86f9a13392b8c8485
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2025-09-29 07:45:05 -0700

    Task1

--- Task1/BankAccount.cs
+++ Task1/BankAccount.cs
@@ -1,1 +1,23 @@
 // TODO Task 1.1 Compose your BankAccount class code in this file
+namespace Banking;
+
+public class BankAccount
+{
+    //Create two properties named AccountNumber and Balance. The AccountNumber property must store strings in the format of "123-456". The Balance property must be of type decimal.
+
+    private string? accountNumber;
+    public required string AccountNumber
+    {
+        get=> accountNumber?? string.Empty;
+        set
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(value, @"^\d{3}-\d{3}$"))
+            {
+                throw new System.ArgumentException("Account number must be in the format '123-456'.");
+            }
+            accountNumber = value;
+        }
+    }
+    public decimal Balance { get; set; }    
+
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,7 +5,11 @@
         {
             // TODO Task 1.2 create an instance of BankAccount class here
             // var account = ...
-            
+            var account = new BankAccount()
+            {
+                AccountNumber = "123-456",
+                Balance = 1000.50m
+            };
             // END OF TODO Task 1.2 - DO NOT MODIFY PAST THIS LINE
             Console.WriteLine($"Account {account.AccountNumber} Balance: {account.Balance:C}");
         }
