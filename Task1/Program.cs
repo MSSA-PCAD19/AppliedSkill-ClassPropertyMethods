8b23d48bb7d3464431d55916289dc6332898b14f
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2025-09-29 07:45:05 -0700

    update output

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,7 +8,7 @@
             var account = new BankAccount()
             {
                 AccountNumber = "123-456",
-                Balance = 1000.50m
+                Balance = 1000.23m
             };
             // END OF TODO Task 1.2 - DO NOT MODIFY PAST THIS LINE
             Console.WriteLine($"Account {account.AccountNumber} Balance: {account.Balance:C}");
